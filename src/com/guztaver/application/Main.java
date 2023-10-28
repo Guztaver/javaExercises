663a487869204ad91624072e69617997f6369ff8
Guztaver <gutadosanjos@gmail.com>
2023-10-28 20:11:56 -0300

    Adding the solutions to Main.java

--- src/com/guztaver/application/Main.java
+++ src/com/guztaver/application/Main.java
@@ -1,9 +1,15 @@
 package com.guztaver.application;
 
+import com.guztaver.vector.solutions.*;
+
 import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
+
+        new FloatSum();
+        new NegativeNumber();
+        new PeopleHeight();
     }
 }
